42734a89f360ad56ddd0432f98693d72e9bf77b6
huayq <u2222223@163.com>
2025-08-04 17:52:02 +0800

    t

--- BD/static/test.js
+++ BD/static/test.js
@@ -1,5 +1,3 @@
-window.$test = function (content) {
-  console.log(content, "test");
-  console.log($, "test");
-  console.log(GM_xmlhttpRequest, "test");
+window.$test = function (newWindow) {
+  console.log(newWindow, "test");
 };
