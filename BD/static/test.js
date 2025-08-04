52ad4d5b40aaeb539e6b21debf2725b73f82e773
huayq <u2222223@163.com>
2025-08-04 18:03:08 +0800

    t

--- BD/static/test.js
+++ BD/static/test.js
@@ -1,3 +1,32 @@
 window.$test = function (newWindow) {
   console.log(newWindow, "test");
 };
+
+// test.js
+(function (global, factory) {
+  if (typeof exports === "object" && typeof module !== "undefined") {
+    // CommonJS/Node 环境 (模块化加载)
+    factory(global, true);
+  } else {
+    // 浏览器环境 (直接加载)
+    factory(global, false);
+  }
+})(typeof window !== "undefined" ? window : this, function (global, isModule) {
+  async function start() {
+    console.log("start function executed", GM_xmlhttpRequest);
+    // 你的逻辑
+  }
+
+  // 暴露接口
+  const lib = { start };
+
+  if (isModule) {
+    // 模块化环境导出
+    if (typeof module !== "undefined" && module.exports) {
+      module.exports = lib;
+    }
+  } else {
+    // 浏览器环境挂载到全局
+    global.$lib = lib;
+  }
+});
